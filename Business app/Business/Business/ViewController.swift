6adf7a0079b7333a575cd4c35c76d2330b0235bf
JTZhangg <69472235+JTZhangg@users.noreply.github.com>
2020-08-10 23:23:15 +0800

    Add files via upload

---
+++ Business app/Business/Business/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Business
+//
+//  Created by Justin Zhang on 8/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
